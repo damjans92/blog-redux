3236ff4a0ce35d31f204b569bba244f9ff8063bb
Damjan Savkovic <damjans92@gmail.com>
2022-04-04 14:43:18 +0200

    lint error fix

--- src/components/Forms/FormAddPost.jsx
+++ src/components/Forms/FormAddPost.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useDispatch } from "react-redux";
 import { addNewPost } from "../../redux/blogSlice";
 import useForm from "../../hooks/useForm";
--- src/components/Header/HeaderBottom.jsx
+++ src/components/Header/HeaderBottom.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import AddPost from "../AddPost";
 import Notification from "./Notification";
 
